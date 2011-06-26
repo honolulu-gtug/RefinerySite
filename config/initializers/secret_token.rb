2bbf5956705ba42a631edac09b7fbc85b36f4322
John Wang <john@johntwang.com>
2011-06-25 16:41:23 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HonoluluGtug::Application.config.secret_token = '5301008b4749790d887f021137b3bc997385e4e3151b6813dc5f748bf4dc622b53b332866375687a72210bfb20b72419fac0f7c61ca154ace93580515f7c01d9'
